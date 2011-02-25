234b3930183a2d5f09ea486bd2ad0568c3799358
lucas.sg.rj@gmail.com <lucas.sg.rj@gmail.com@dca2fcc5-2d94-9552-50b5-93d493856ced>
2011-02-25 01:47:05 +0000

    Projetos C#

---
+++ trunk/Projetos_C#/NHibernate_Web/NHibernate_Web/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NHibernate_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Cadastrar(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+
+            usuario._nome = txt_nome.Text;
+            usuario._login = txt_login.Text;
+            usuario._senha = txt_senha.Text;
+            usuario._perfil = txt_perfil.Text;
+            usuario._email = txt_email.Text;
+
+            UsuarioRepositorio usuarioRepo = new UsuarioRepositorio();
+            usuarioRepo.Add(usuario);
+            
+        }
+    }
+}
